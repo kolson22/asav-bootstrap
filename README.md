22784699c528172d8efbc77ed24dc3d7a16dadef
kolson22 <kyle.olson22@gmail.com>
2022-01-17 18:00:05 -0600

    added readme and worked on args

---
+++ README.md
@@ -0,0 +1,12 @@
+# ASAv Bootstrap
+
+## Getting Started
+
+``` bash
+pip install -r requirements.txt
+```
+
+## Next Steps
+- [ ] jinja templating to generate the configuration
+- [ ] folder structure created to be placed in the iso
+- [ ] testing the iso on a lab ASAv
--- configurator.py
+++ configurator.py
@@ -4,24 +4,28 @@ try:
 except ImportError:
     print("you need to make sure pycdlib and argparse are installed")
 
+import os
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="The thing you want to echo, dummy")
-parser.add_argument("-v", "--verbose",
-                    help="The thing you want to echo, dummy",
-                    action="store_true")
 parser.add_argument("-a", "--address",
-                    help="The IP address of the firewall")
+                    help="The IP address of the firewall",
+                    required=True)
+parser.add_argument("-m", "--mask",
+                    help="The subnet mask, ex: 255.255.255.0",
+                    required=True)
+parser.add_argument("-f", "--filename",
+                    help="The filename to output the iso to, ex: firewall.iso",
+                    required=True)
 args = parser.parse_args()
-if args.verbose:
-    print("super verbose mode activated")
 
-f = open('test.txt', 'w')
-f.write(args.echo)
+f = open("test.txt", "w")
+f.write(args.address + "/" + args.mask)
+f.write("\n")
 f.close()
 
 iso = pycdlib.PyCdlib()
 iso.new(joliet=3)
-foostr = b'foo\n'
-iso.add_file('test.txt', joliet_path="/test.txt")
-iso.write("test.iso")
+iso.add_file("test.txt", joliet_path="/test.txt")
+iso.write(args.filename)
 iso.close()
+
+os.remove("./test.txt")
