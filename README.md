6d18fc15a90a360b1d1ad398313d37fdc82d77ef
Kyle Olson <kyle.olson22@gmail.com>
2022-01-18 13:35:07 -0600

    working example, needs to be tested on ASA

--- README.md
+++ README.md
@@ -6,7 +6,26 @@
 pip install -r requirements.txt
 ```
 
+Modify the './templates/asa.jinja2' file to match the configuration you like'
+
+
+## How to use once configured
+
+``` bash
+configurator.py -a 10.0.0.1 -m 255.255.255.0 -g 10.0.0.254 -n test-firewall -v 9.7.1 -f test-firewall.iso -s 8.8.8.8 -t 1G
+```
+
+- "-a" or "--address" is for the outside IP address of GigabitEthernet0/0
+- "-m" or "--mask" is for the subnet mask for GigabitEthernet0/0
+- "-g" or "--gateway" is the gateway for the outside interface
+- "-n" or "--name" is the hostname of the firewall
+- "-v" or "--version" is the version of the ASAv
+- "-s" or "--ssh" is the IP you want the whitelist for SSH to the outside
+- "-t" or "--throughput" is the throughput level you want to license (100M or 1G)
+- "-i" or "--idtoken" is the idtoken you want to register it to, OPTIONAL
+- "-f" or "--filename" is the filename you want to output to ex: "test-firewall.iso"
+
 ## Next Steps
-- [ ] jinja templating to generate the configuration
-- [ ] folder structure created to be placed in the iso
+- [x] jinja templating to generate the configuration
+- [x] folder structure created to be placed in the iso
 - [ ] testing the iso on a lab ASAv
--- configurator.py
+++ configurator.py
@@ -1,33 +1,80 @@
+#!/usr/bin/env python3
+
 try:
     import argparse
     import pycdlib
+    import os
+    from jinja2 import Template
 except ImportError:
     print("you need to make sure pycdlib and argparse are installed")
 
-import os
-
-test_filename = "./test.txt"
+# get the arguments from the cli
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--address",
-                    help="The IP address of the firewall",
+                    help="The IP address of the outside of the firewall",
                     required=True)
 parser.add_argument("-m", "--mask",
                     help="The subnet mask, ex: 255.255.255.0",
                     required=True)
+parser.add_argument("-g", "--gateway",
+                    help="The default gateway of the outside",
+                    required=True)
+parser.add_argument("-n", "--name",
+                    help="The hostname of the firewall",
+                    required=True)
+parser.add_argument("-v", "--version",
+                    help="The version of the ASA",
+                    required=True)
+parser.add_argument("-s", "--ssh",
+                    help="The IP you will need whitelisted for SSH",
+                    required=True)
+parser.add_argument("-t", "--throughput",
+                    help="The throughput of ASAv, ex: 1G or 100M",
+                    required=True)
+parser.add_argument("-i", "--idtoken",
+                    help="The id token for smart licensing: OPTIONAL")
 parser.add_argument("-f", "--filename",
                     help="The filename to output the iso to, ex: firewall.iso",
                     required=True)
 args = parser.parse_args()
 
-f = open("test.txt", "w")
-f.write(args.address + "/" + args.mask)
-f.write("\n")
-f.close()
+# open the template file to build the configuration
+with open('./templates/asa.jinja2') as template_file:
+    template = Template(template_file.read())
 
+# make a rendered configuration with the args as variables
+config_output = template.render(
+    address=args.address,
+    mask=args.mask,
+    gateway=args.gateway,
+    hostname=args.name,
+    version=args.version,
+    ssh_allow=args.ssh,
+    model=args.throughput
+)
+
+# write the rendered configuration to the day0 config file
+config_file = open("day0-config", "w")
+config_file.write(config_output)
+config_file.close()
+
+# if idtoken is present write it to a file
+if args.idtoken:
+    token_file = open("idtoken", "w")
+    token_file.write(args.idtoken)
+    token_file.close()
+
+# create the iso object and place the files in there
 iso = pycdlib.PyCdlib()
-iso.new(joliet=3)
-iso.add_file(test_filename, joliet_path="/test.txt")
+iso.new(joliet=3, vol_ident=args.name.upper())
+iso.add_file("day0-config", joliet_path="/day0-config")
+if args.idtoken:
+    iso.add_file("idtoken", joliet_path="/idtoken")
 iso.write(args.filename)
 iso.close()
 
-os.remove(test_filename)
+# clean up the config and token file
+os.remove("day0-config")
+if args.idtoken:
+    os.remove("idtoken")
+print("Successfully created the boostrap iso: ./" + args.filename)
